d30c34aa297e794d92c060dea43d84429bc8eacc
Efrain Perez <efrain.abperez23@gmail.com>
2025-10-05 00:59:28 -0500

    feat: changed only dispatch github actions trigger

--- core/s3/s3.go
+++ core/s3/s3.go
@@ -8,20 +8,23 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/olekukonko/tablewriter"
 )
 
-var s3Client *s3.Client
-
+// isBucketEmpty checks if a bucket is empty by listing the objects in the bucket and checking if the list is empty.
+// If the bucket name is empty, it will panic with the error "Bucket name is empty".
+// If there is an error listing the objects, it will panic with the error "Error listing objects for bucket <bucketName>: <error>".
+// Returns true if the bucket is empty, false otherwise.
 func isBucketEmpty(bucketName string) bool {
+
+	InitS3Client()
 
 	if bucketName == "" {
 		panic("Bucket name is empty")
 	}
 
-	output, err := s3Client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+	output, err := S3Client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
 		Bucket:  aws.String(bucketName),
 		MaxKeys: aws.Int32(1),
 	})
@@ -34,20 +37,14 @@ func isBucketEmpty(bucketName string) bool {
 // CheckEmpytBuckets lists all the buckets in your AWS account and checks if they are empty.
 // It renders a table with the bucket name and a symbol indicating if the bucket is empty or not.
 func CheckEmpytBuckets() {
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		panic(fmt.Sprintf("unable to load SDK config, %v", err))
-	}
 
-	s3Client = s3.NewFromConfig(cfg)
+	InitS3Client()
 
-	buckets, err := s3Client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
-	if err != nil {
-		panic(fmt.Sprintf("Error listing buckets: %v", err))
-	}
+	buckets := GetAllS3SBuckets()
 
 	table := tablewriter.NewWriter(os.Stdout)
 	table.Header([]string{"Bucket Name", "Empty"})
+
 	for _, b := range buckets.Buckets {
 		bucketName := aws.ToString(b.Name)
 
